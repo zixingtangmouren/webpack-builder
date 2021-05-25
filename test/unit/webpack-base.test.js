ff1dff896044cb253728afc3550bfb6fe9fc3223
tamgmouren <83946821+zixingtangmouren@users.noreply.github.com>
2021-05-25 10:42:03 +0800

    update

--- test/unit/webpack-base.test.js
+++ test/unit/webpack-base.test.js
@@ -2,7 +2,7 @@
  * @Author: tangzhicheng
  * @Date: 2021-05-24 22:46:53
  * @LastEditors: tangzhicheng
- * @LastEditTime: 2021-05-24 22:57:42
+ * @LastEditTime: 2021-05-25 10:41:56
  * @Description: file content
  */
 
@@ -15,5 +15,13 @@ describe('webpack.base test case', () => {
   it('entry', () => {
     // if (baseConfig.entry)
     assert.deepStrictEqual(baseConfig.entry, { index: './src/index/index.js' })
+  })
+
+  it('html plugins', (done) => {
+    if (baseConfig.plugins.length > 0) {
+      done()
+    } else {
+      throw new Error('create plugin error')
+    }
   })
 })
