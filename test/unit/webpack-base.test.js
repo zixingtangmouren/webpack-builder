8cfe0d88151aa42ca00caa8b1958f23771b8267d
tamgmouren <83946821+zixingtangmouren@users.noreply.github.com>
2021-05-25 16:46:08 +0800

    fix

--- test/unit/webpack-base.test.js
+++ test/unit/webpack-base.test.js
@@ -2,7 +2,7 @@
  * @Author: tangzhicheng
  * @Date: 2021-05-24 22:46:53
  * @LastEditors: tangzhicheng
- * @LastEditTime: 2021-05-25 16:27:28
+ * @LastEditTime: 2021-05-25 16:45:47
  * @Description: file content
  */
 
@@ -14,7 +14,7 @@ describe('webpack.base test case', () => {
 
   it('entry', () => {
     // if (baseConfig.entry)
-    assert.deepStrictEqual(baseConfig.entry, { index: './src/index/index.jsx' })
+    assert.deepStrictEqual(baseConfig.entry, { index: './src/index/index.js' })
   })
 
   it('html plugins', (done) => {
