39b84b92ef056920fdde6ffa775751041f91dfdd
tamgmouren <83946821+zixingtangmouren@users.noreply.github.com>
2021-05-28 15:30:39 +0800

    增加css的树摇

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@
  * @Author: tangzhicheng
  * @Date: 2021-05-24 22:45:33
  * @LastEditors: tangzhicheng
- * @LastEditTime: 2021-05-28 11:51:01
+ * @LastEditTime: 2021-05-28 15:16:50
  * @Description: file content
  */
 
@@ -14,6 +14,6 @@ process.chdir(path.join(__dirname, 'smoke/template'))
 describe('checking weboack-config', () => {
   // require('./unit/webpack-base.test')
   // require('./unit/webpack-dll.test')
-  // require('./unit/webpack-prod.test')
-  require('./unit/webpack-dev.test')
+  require('./unit/webpack-prod.test')
+  // require('./unit/webpack-dev.test')
 })
--- test/smoke/template/src/index/App.jsx
+++ test/smoke/template/src/index/App.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
+import './style.scss'
 
 const App = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="app">
       asdasd
       <button onClick={() => setCount(100)}>click me</button>
-      <div>{count}</div>
-    </>
+      <div className="niu-plus">{count}</div>
+    </div>
   )
 }
 
