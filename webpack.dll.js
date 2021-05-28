028bf4107d46e8190ef6ac061afeacf2cebf8190
tamgmouren <83946821+zixingtangmouren@users.noreply.github.com>
2021-05-28 09:49:19 +0800

    init

--- webpack.dll.js
+++ webpack.dll.js
@@ -2,15 +2,13 @@
  * @Author: tangzhicheng
  * @Date: 2021-05-26 09:20:09
  * @LastEditors: tangzhicheng
- * @LastEditTime: 2021-05-26 15:49:32
+ * @LastEditTime: 2021-05-28 09:48:16
  * @Description: 预编译文件
  */
 
 const webpack = require('webpack')
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-
-const CURRENTDIR = process.cwd()
 
 module.exports = {
   entry: {
@@ -21,15 +19,15 @@ module.exports = {
   },
   output: {
     filename: '[name]_[chunkhash:8].dll.js',
-    path: path.join(CURRENTDIR, 'library'),
+    path: path.join(__dirname, 'library'),
     library: '[name]',
   },
   plugins: [
     new CleanWebpackPlugin(),
     new webpack.DllPlugin({
-      context: CURRENTDIR,
+      context: __dirname,
       name: '[name]_[chunkhash:8]',
-      path: path.join(CURRENTDIR, 'library/manifest.json'),
+      path: path.join(__dirname, 'library/manifest.json'),
     }),
   ],
 }
