2e26d0acece17d99d623e0b6052bae5af7ca2bf3
tamgmouren <83946821+zixingtangmouren@users.noreply.github.com>
2021-06-15 10:54:52 +0800

    修改entry配置入口

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
+<!--
+ * @Author: tangzhicheng
+ * @Date: 2021-05-28 11:48:30
+ * @LastEditors: tangzhicheng
+ * @LastEditTime: 2021-06-15 10:47:18
+ * @Description: file content
+-->
+
 # webpack-builder
-a webpack generate
+
+1. 修改 entry 的合理性
+2. 控制 libary 输出位置
--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@
  * @Author: tangzhicheng
  * @Date: 2021-05-24 22:45:33
  * @LastEditors: tangzhicheng
- * @LastEditTime: 2021-06-14 16:44:03
+ * @LastEditTime: 2021-06-15 10:36:03
  * @Description: file content
  */
 
@@ -13,7 +13,7 @@ process.chdir(path.join(__dirname, 'smoke/template'))
 
 describe('checking weboack-config', () => {
   // require('./unit/webpack-base.test')
-  require('./unit/webpack-dll.test')
-  // require('./unit/webpack-prod.test')
+  // require('./unit/webpack-dll.test')
+  require('./unit/webpack-prod.test')
   // require('./unit/webpack-dev.test')
 })
