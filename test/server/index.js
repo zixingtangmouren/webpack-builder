b2ba3914a8e7ab1680e9d91bca2540360e3cc038
tamgmouren <83946821+zixingtangmouren@users.noreply.github.com>
2021-06-15 14:34:16 +0800

    fix

---
+++ test/server/index.js
@@ -0,0 +1,28 @@
+/**
+ * @Author: tangzhicheng
+ * @Date: 2021-06-15 14:17:26
+ * @LastEditors: tangzhicheng
+ * @LastEditTime: 2021-06-15 14:30:02
+ * @Description: file content
+ */
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const ROOT = process.cwd()
+
+console.log(ROOT)
+
+// // 匹配任意请求*, 并且放到express.static 静态服务器的前面。
+// app.get('*', (req, res) => {
+//   res.send('Hello word')
+// })
+
+app.use(express.static(path.join(ROOT, 'test/smoke/template/dist')))
+
+
+
+
+app.listen(5000, () => {
+  console.log('start: 5000')
+})
