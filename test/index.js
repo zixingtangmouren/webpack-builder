3e5d9db76da8c8560f6a08966ecfa1af2302ec20
tamgmouren <83946821+zixingtangmouren@users.noreply.github.com>
2021-06-18 21:31:35 +0800

    fix:更新配置

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@
  * @Author: tangzhicheng
  * @Date: 2021-05-24 22:45:33
  * @LastEditors: tangzhicheng
- * @LastEditTime: 2021-06-18 17:08:25
+ * @LastEditTime: 2021-06-18 21:29:23
  * @Description: file content
  */
 
@@ -13,6 +13,6 @@ process.chdir(path.join(__dirname, 'smoke/template'))
 
 describe('checking weboack-config', () => {
   // require('./unit/webpack-base.test')
-  // require('./unit/webpack-prod.test')
-  require('./unit/webpack-dev.test')
+  require('./unit/webpack-prod.test')
+  // require('./unit/webpack-dev.test')
 })
