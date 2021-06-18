f21356532ddefcd3450ab1118b083a750c0d2f03
tamgmouren <83946821+zixingtangmouren@users.noreply.github.com>
2021-06-18 21:49:40 +0800

    fix

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+/**
+ * @Author: tangzhicheng
+ * @Date: 2021-06-18 21:48:06
+ * @LastEditors: tangzhicheng
+ * @LastEditTime: 2021-06-18 21:48:36
+ * @Description: file content
+ */
+
+module.exports = {
+  plugins: [
+    require('autoprefixer'),
+  ],
+}
