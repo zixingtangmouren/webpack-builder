e65548c099c3745f59a917042446ecb88721ea06
tamgmouren <83946821+zixingtangmouren@users.noreply.github.com>
2021-06-18 22:00:31 +0800

    fx

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,10 +2,9 @@
  * @Author: tangzhicheng
  * @Date: 2021-06-15 21:11:54
  * @LastEditors: tangzhicheng
- * @LastEditTime: 2021-06-16 20:35:22
+ * @LastEditTime: 2021-06-18 21:58:35
  * @Description: file content
  */
-
 
 const { devConfig } = require('webpack-v4-builder')
 
